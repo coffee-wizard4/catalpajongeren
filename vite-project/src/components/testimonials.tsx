d3f7d6286d16b920d339b5f8389afb620713175b
coffee-wizard4 <kobe.tjoen@gmail.com>
2024-11-10 10:10:10 +0100

    organized project and created starter testimonials

---
+++ vite-project/src/components/testimonials.tsx
@@ -0,0 +1,10 @@
+import { motion } from "framer-motion"
+export const Testimonials : React.FC = () => {
+    return (
+        <>
+        <motion.div>
+            
+        </motion.div>
+        </>
+    )
+}
